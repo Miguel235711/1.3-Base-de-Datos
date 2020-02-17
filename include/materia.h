078c3fa2718cb9e1d3c555d96b5abf727a799559
Miguel235711 <a01423629@itesm.mx>
2020-02-17 14:40:43 -0600

    adding materia class

---
+++ include/materia.h
@@ -0,0 +1,16 @@
+#ifndef MATERIA_H
+#define MATERIA_H
+
+
+class materia
+{
+    public:
+        materia();
+        virtual ~materia();
+
+    protected:
+
+    private:
+};
+
+#endif // MATERIA_H
---
+++ src/materia.cpp
@@ -0,0 +1,11 @@
+#include "materia.h"
+
+materia::materia()
+{
+    //ctor
+}
+
+materia::~materia()
+{
+    //dtor
+}
