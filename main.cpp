ea65de3cf9714bc5200f5585b7fd7466b8090366
Miguel235711 <a01423629@itesm.mx>
2020-02-18 01:25:53 -0600

    complete

--- main.cpp
+++ main.cpp
@@ -1,29 +1,17 @@
 #include <iostream>
 #include "include/profesor.h"
+#include "include/materia.h"
 using namespace std;
 
 int main()
 {
-    int centinela, opcion;
-
-    profesor misa;
-
-  do{
-        cout<<"Escribe 1 para agregar nombres"<<endl;
-        cin>>opcion;
-        cout<<"Escribe 2 para mostrar los nombres"<<endl;
-        cin >>opcion;
-        cout<<"Escribe -1 para salir"<<endl;
-        cin>> centinela;
-        if(opcion == 1) misa.agregar_nombres();
-        if(opcion==2) misa.mostrar_nombres();
-    }while(centinela != -1);
-
-   // outfile.close();
-    //infile.close();
+    materia laMateria;
+    laMateria.loadFile();
+    //laMateria.mostrarMaterias();
+    //laMateria.borrarMateria("1");
+    //laMateria.agregarMateria();
+    //laMateria.editarMateria("1");
     return 0;
-
-
 }
 
 
